c375ab1c55218e8749023ecac51ebed625c0813c
chipmunk-tail <j1997m@naver.com>
2024-12-18 16:58:54 +0900

    simple correction

--- job01_crawling_headline.py
+++ job01_crawling_headline.py
@@ -1,4 +1,5 @@
-# 241218
+# 241218 intel AISW Academy
+#
 
 from bs4 import BeautifulSoup
 import requests
@@ -13,19 +14,19 @@ category = ['Politics', 'Economic', 'Social', 'Culture', 'World', 'IT']
 df_titles = pd.DataFrame()
 
 # Get headlines
-for i in range(6):                                              # Domain use 100 ~ 105 subdomain
+for i in range(6):                                              # Domain use 100 ~ 105 sub_domain
     url = 'https://news.naver.com/section/10{}'. format(i)      # OR '/10%d' %i
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, 'html.parser')      # Using bs4 for HTML parsing
+    resp = requests.get(url)                                    # url request == HTML
+    soup = BeautifulSoup(resp.text, 'html.parser')          # Using bs4 for HTML parsing
     title_tags = soup.select('.sa_text_strong')                 # '.sa_text_strong' == news headline class def name
-    titles = []                                                 # Empty list for
+    titles = []                                                 # Create empty list for save headline
 
     for title_tag in title_tags:                                # all headlines
-        title = title_tag.text
-        title = re.compile('[^가-힣 ]').sub('', title)      # Repalce all to 'null' execpt "가 ~ 힣" && " "
+        title = title_tag.text                                  # Crawling headlines
+        title = re.compile('[^가-힣 ]').sub('', title)        # Repalce all to 'null' execpt "가 ~ 힣" && " "
         titles.append(title)
 
-    df_section_titles = pd.DataFrame(titles, columns = ['titles'])
+    df_section_titles = pd.DataFrame(titles, columns = ['titles'])  # Create columns 'titles'
     df_section_titles['category'] = category[i]
     df_titles = pd.concat([df_titles, df_section_titles], axis = 'rows', ignore_index = True)
 
@@ -34,7 +35,7 @@ df_titles.info()
 print(df_titles['category'].value_counts())
 df_titles.to_csv('./crawling_data/naver_headline_news_{}.csv'.format(
     datetime.datetime.now().strftime('%Y%m%d')), index = False) # Change time format to 'YYYYMMDD'
-                                                                # index = False ==
+                                                                # index = False == 0, 1, 2 default index = False
 
 
 
